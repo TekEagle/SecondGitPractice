13b88efc788d84edb3f9b2eaf37431e04f586f9b
TekEagle <halimah.rafat@gmail.com>
2021-07-27 12:19:32 -0400

    Second Practice Code

---
+++ src/main/java/com/EAGLE/bdd/framework/com/EAGLE/bdd/framework/App.java
@@ -0,0 +1,13 @@
+package com.EAGLE.bdd.framework.com.EAGLE.bdd.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
